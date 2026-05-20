175177c52da1357fe7b07672438387c54f8c126b
nekruz2000n-eng <nekruz2000n@gmail.com>
2026-05-20 21:43:16 +0700

    Update QuestionsTab.tsx

--- src/components/QuestionsTab.tsx
+++ src/components/QuestionsTab.tsx
@@ -485,7 +485,7 @@ export const QuestionsTab = ({ onSecretTap, subject = 'ortho' }: { onSecretTap?:
     let x = (vw / 2) - (popup.width / 2);
 
     setTooltipPos({ x, y });
-  }, [activeTermDef, tooltipTarget]);
+    }, [tooltipTarget]);
 
   // ЗАКРЫТИЕ СТЕКА ПО КЛИКУ
   useEffect(() => {
