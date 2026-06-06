0a872a80b0f9fec0888bdda7303bbe38b8b183a9
nekruz <nekruz2000n@gmail.com>
2026-06-06 23:07:54 +0700

    Fix build error in payment subject update handler.

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -662,7 +662,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       await saveUser(tgIdStr, touchUserActivity(switched));
       return res.status(200).json({
         success: true,
-        ...(await subjectsResponse(updated, tgIdStr)),
+        ...(await subjectsResponse(switched, tgIdStr)),
       });
     }
 
